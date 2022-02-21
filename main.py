73d512ead794e90c4ca4d1a45d2e9ac8433a9bfe
blue-horizons <jojotraingeek@gmail.com>
2022-02-21 17:14:19 +0000

    edits to the orentation of the output file

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ while count <= 999:
 
     #-- Set xml version/ encoding etc
     svgOut = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg height="86" width="86">
+<svg height="1600" width="1600">
 """
 
     #generate random colour
@@ -50,7 +50,7 @@ while count <= 999:
     backgroundColour = colourGen(None)
 
     background = f"""<!-- Background -->
-    <path d="M 0 0 L 80 0 L 80 80 L 0 80 Z"
+    <path d="M 0 0 L 1600 0 L 1600 1600 L 0 1600 Z"
     fill="{backgroundColour}" stroke="Black" stroke-width="0px"/>"""
     
     svgOut = svgOut + background
@@ -61,7 +61,7 @@ while count <= 999:
 
     face = f"""
     <!-- Face -->
-    <path d="M 15 15 L 19 4 L 23 15 L 27 4 L 31 15 L 61 15 C 63 15 65 17 65 19 L 65 39 L 39 39 C 38 39 37 40 37 41 L 37 58 C 37 59 38 60 39 60 L 65 60 C 65 63 63 65 60 65 L 20 65 C 17 65 15 63 15 60 Z M 0 0 L 80 0 L 80 80 L 0 80 Z M 16 15 L 19 6 L 22 15 Z" 
+    <path d="M 300 300 L 380 80 L 460 300 L 540 80 L 620 300 L 1220 300 C 1260 300 1300 340 1300 380 L 1300 780 L 780 780 C 760 780 740 800 740 820 L 740 1160 C 740 1180 760 1200 780 1200 L 1300 1200 C 1300 1260 1260 1300 1200 1300 L 400 1300 C 340 1300 300 1260 300 1200 Z" 
     fill="{furColour}" stroke="black" stroke-width="0.5" />"""
     
     svgOut = svgOut + face
@@ -70,10 +70,18 @@ while count <= 999:
 
     body = f"""
     <!-- Body -->
-    <path d="M 24 15 L 27 6 L 30 15 Z M 15 45 L -1 45 L -1 82 L 25 82 L 25 65 L 20 65 C 17 65 15 63 15 60 Z" 
+    <path d="M 300 1100 L -20 1100 L -20 1620 L 540 1620 L 540 1300 L 400 1300 C 340 1300 300 1260 300 1200 Z" 
     fill="{furColour}" stroke="black" stroke-width="0.5" />"""
 
     svgOut = svgOut + body
+
+    #-- Ears --
+
+    ears = """<!-- Ears -->
+    <path d="M 320 300 L 380 140 L 440 300 Z M 480 300 L 540 140 L 600 300 Z"
+    fill="#ffbec1" stroke="black" stroke-width="0.5" />"""
+
+    svgOut = svgOut + ears
 
     #-- Face Spots --
 
@@ -106,13 +114,13 @@ while count <= 999:
 
     eyes = f"""
     <!-- Eyes -->
-    <!-- Whites -->
-    <path d="M -13 -15 C -13 -18 -12 -21 -9 -21 C -6 -21 -5 -18 -5 -15 C -5 -12 -6 -9 -9 -9 C -12 -9 -13 -12 -13 -15 C -13 -18 -14 -21 -17 -21 C -20 -21 -21 -18 -21 -15 C -21 -12 -20 -9 -17 -9 C -14 -9 -13 -12 -13 -15 Z"
+    <!-- Outer -->
+    <path d="M 540 500 C 540 440 560 380 620 380 C 680 380 700 440 700 500 C 700 560 680 620 620 620 C 560 620 540 560 540 500 C 540 440 520 380 460 380 C 400 380 380 440 380 500 C 380 560 400 620 460 620 C 520 620 540 560 540 500 Z"
         fill="{eyeColour}" stroke="black" stroke-width="0.5"/>
     <!-- Pupils -->
-    <path d="M -18 -14 C -18 -15 -18 -17 -16 -17 C -14 -17 -14 -15 -14 -14 C -14 -13 -14 -11 -16 -11 C -18 -11 -18 -13 -18 -14 Z"
+    <path d="M 440 520 C 440 500 440 460 480 460 C 520 460 520 500 520 520 C 520 540 520 580 480 580 C 440 580 440 540 440 520 Z"
         fill="black" stroke="black" stroke-width="0.5"/>
-    <path d="M -10 -14 C -10 -15 -10 -17 -8 -17 C -6 -17 -6 -15 -6 -14 C -6 -13 -6 -11 -8 -11 C -10 -11 -10 -13 -10 -14 Z"
+    <path d="M 600 520 C 600 500 600 460 640 460 C 680 460 680 500 680 520 C 680 540 680 580 640 580 C 600 580 600 540 600 520 Z"
         fill="black" stroke="black" stroke-width="0.5"/>"""    
     
     svgOut = svgOut + eyes
