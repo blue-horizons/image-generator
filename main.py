681c99c6a3fac0a19cc25ca4db8295291b3ef6f1
blue-horizons <jojotraingeek@gmail.com>
2022-02-21 14:30:04 +0000

    edits to svg format

--- main.py
+++ main.py
@@ -40,8 +40,13 @@ while count <= 999:
 
     #-- Background --
 
-    background = colourGen(None)
-    print("background colour is " + background)
+    backgroundColour = colourGen(None)
+
+    background = f"""<!-- Background -->
+    <path d="M -40 -40 L 40 -40 L 40 40 L -40 40 Z"
+        fill="{backgroundColour} />"""
+    
+    svgOut = svgOut + background
 
     #-- Face --
 
@@ -49,12 +54,11 @@ while count <= 999:
 
     face = f"""
     <!-- Face -->
-    <path d="M 19 19 L 23 8 L 27 19 L 31 8 L 35 19 L 63 19 C 65 19 67 21 67 23 L 67 43 L 43 43 C 42 43 41 44 41 45 L 41 57 C 41 58 42 59 43 59 L 67 59 C 67 61 65 63 63 63 L 23 63 C 21 63 19 61 19 59 Z" 
+    <path d="M -25 -25 L -21 -36 L -17 -25 L -13 -36 L -9 -25 L 21 -25 C 23 -25 25 -23 25 -21 L 25 -1 L -1 -1 C -2 -1 -3 0 -3 1 L -3 18 C -3 19 -2 20 -1 20 L 25 20 C 25 23 23 25 20 25 L -20 25 C -23 25 -25 23 -25 20 Z" 
     fill="{furColour}" stroke="black" stroke-width="0.5" />"""
     
     svgOut = svgOut + face
-
-
+    
     #-- Body --
 
     body = f"""
@@ -91,6 +95,25 @@ while count <= 999:
         svgOut = svgOut + spots
     else:
         None
+
+
+    #-- Eyes --
+
+    eyeColour = colourGen(None)
+
+    eyes = f"""<!-- Eyes -->
+    <!-- Whites -->
+    <path d="M -13 -15 C -13 -18 -12 -21 -9 -21 C -6 -21 -5 -18 -5 -15 C -5 -12 -6 -9 -9 -9 C -12 -9 -13 -12 -13 -15 C -13 -18 -14 -21 -17 -21 C -20 -21 -21 -18 -21 -15 C -21 -12 -20 -9 -17 -9 C -14 -9 -13 -12 -13 -15 Z"
+        fill={eyeColour} stroke="black"/>
+    <!-- Pupils -->
+    <path d="M -18 -14 C -18 -15 -18 -17 -16 -17 C -14 -17 -14 -15 -14 -14 C -14 -13 -14 -11 -16 -11 C -18 -11 -18 -13 -18 -14 Z"
+        fill="black" stroke="black"/>
+    <path d="M -10 -14 C -10 -15 -10 -17 -8 -17 C -6 -17 -6 -15 -6 -14 C -6 -13 -6 -11 -8 -11 C -10 -11 -10 -13 -10 -14 Z"
+        fill="black" stroke="black"/>"""    
+    
+    svgOut = svgOut + eyes
+
+    #-- File Writing + Finalising --
 
     svgOut = svgOut + "\n</svg>"
     print(svgOut)
